b69e73063863b7623714939bbde656573aeee266
MrManiacc <sinfulcynic@yahoo.com>
2023-08-24 20:48:59 -0400

    Initial project setup ✅ `core subsystems in place`

---
+++ app/src/app.c
@@ -0,0 +1,9 @@
+/**
+ * Created by jraynor on 8/24/2023.
+ */
+#include "defines.h"
+
+
+int main(void){
+    return 0;
+}
---
+++ app/src/app.h
@@ -0,0 +1,4 @@
+/**
+ * Created by jraynor on 8/24/2023.
+ */
+#pragma once
