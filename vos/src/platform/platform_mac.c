f33a182520e305b0991ad1918a6b9839cdf1564d
James <vtboy2000@gmail.com>
2024-02-13 23:32:34 -0500

    Fully working on macos, might still need some defines for default windows behavior. will check tomorrow.

--- vos/src/platform/platform_mac.c
+++ vos/src/platform/platform_mac.c
@@ -225,11 +225,7 @@ void platform_console_write(const char *message, u8 colour) {
 }
 
 void platform_console_write_error(const char *message, u8 colour) {
-    // FATAL,ERROR,WARN,INFO,DEBUG,TRACE
-//    static const char *colour_strings[] = {"0;41", "1;31", "1;33", "1;32", "1;34", "1;30"};
-//    printf("\033[%sm%s\033[0m", colour_strings[colour], message);
-    printf("%s", message);
-
+    platform_console_write(message, colour);//just for now ?
 }
 
 
